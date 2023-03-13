7c84e11971ae8a3382d0bca5912b9072ce889aa6
tibudiaz <117239415+tibudiaz@users.noreply.github.com>
2023-03-13 15:09:53 -0300

    actualizacion

--- assets/calculadora.js
+++ assets/calculadora.js
@@ -1,21 +1,21 @@
 function calcularCuotas() {
-    // Obtener el valor ingresado por el usuario
+    // Obtener el valor1 ingresado por el usuario
     var valor = parseFloat(document.getElementById("valor").value);
-  
-    // Calcular las cuotas
-    var cuota3 = valor / 3;
-    var cuota6 = valor / 6;
-    var cuota12 = valor / 12;
-  
+    var valor1 = valor + 10000;
     // Calcular los intereses
-    var interes3 = valor * 0.2051097;
-    var interes6 = valor * 0.3344009;
-    var interes12 = valor * 0.6369291;
+    var interes3 = valor1 * 0.2051097;
+    var interes6 = valor1 * 0.3344009;
+    var interes12 = valor1 * 0.6369291;
   
     // Calcular los totales y convertirlos a números enteros
-    var total3 = Math.round((valor + interes3) / 1);
-    var total6 = Math.round((valor + interes6) / 1);
-    var total12 = Math.round((valor + interes12) / 1);
+    var total3 = Math.round((valor1 + interes3) / 1);
+    var total6 = Math.round((valor1 + interes6) / 1);
+    var total12 = Math.round((valor1 + interes12) / 1);
+
+    // Calcular las cuotas
+    var cuota3 = total3 / 3;
+    var cuota6 = total6 / 6;
+    var cuota12 = total12 / 12;
   
     // Mostrar los resultados en la página web con formato de moneda y números enteros
     document.getElementById("resultado3").textContent = "3 cuotas de: " + cuota3.toLocaleString('es-AR', {style: 'currency', currency: 'ARS'}) + " (total: " + total3.toLocaleString('es-AR', {style: 'currency', currency: 'ARS'}) + ")";
